33173f6c94b1d07cc9402bf4ec479693f59bfbe0
ABedychaj <andrzej.bedychaj@karhoo.com>
2022-09-29 10:39:54 +0200

    AE_training

--- StyleGAN/train_ae.py
+++ StyleGAN/train_ae.py
@@ -1,159 +1,134 @@
-import argparse
-import numpy as np
+import pickle
+import dnnlib
+import dnnlib.tflib as tflib
+
+from sklearn.utils import shuffle
+from datetime import datetime
+
 import torch
-from torch.distributions import Normal
 import torch.optim as optim
 
 from AutoEncoder import AE_single_layer
-from module.flow import cnf
-from datetime import datetime
-from torch import nn
-from sklearn.utils import shuffle
-from NICE import NiceFlow
+from utils import load_dataset, save_model
 
-from utils import load_dataset, make_dir, iterate_batches, load_model, save_model
+# only ffhq
+gdrive_urls = {
+    'gdrive:networks/stylegan2-ffhq-config-a.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-a.pkl',
+    'gdrive:networks/stylegan2-ffhq-config-b.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-b.pkl',
+    'gdrive:networks/stylegan2-ffhq-config-c.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-c.pkl',
+    'gdrive:networks/stylegan2-ffhq-config-d.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-d.pkl',
+    'gdrive:networks/stylegan2-ffhq-config-e.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-e.pkl',
+    'gdrive:networks/stylegan2-ffhq-config-f.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-f.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gorig-Dorig.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gorig-Dorig.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gorig-Dresnet.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gorig-Dresnet.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gorig-Dskip.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gorig-Dskip.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gresnet-Dorig.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gresnet-Dorig.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gresnet-Dresnet.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gresnet-Dresnet.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gresnet-Dskip.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gresnet-Dskip.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gskip-Dorig.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gskip-Dorig.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gskip-Dresnet.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gskip-Dresnet.pkl',
+    'gdrive:networks/table2/stylegan2-ffhq-config-e-Gskip-Dskip.pkl': 'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gskip-Dskip.pkl',
+}
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--layers", type=int, default=4)
-parser.add_argument("--values", type=str, default="continuous")
-parser.add_argument("--sigma", type=float, default=0.4)
-parser.add_argument("--decay", type=float, default=0.999)
-parser.add_argument("--age-sigma", type=float, default=0.3)
-parser.add_argument("--start-epoch", type=int, default=0)
-parser.add_argument("--lr", type=float, default=1e-4)
-args = parser.parse_args()
 
-torch.random.manual_seed(123)
+def get_path_or_url(path_or_gdrive_path):
+    return gdrive_urls.get(path_or_gdrive_path, path_or_gdrive_path)
+
+
+# ----------------------------------------------------------------------------
+
+
+_cached_networks = dict()
+
+
+def load_networks(path_or_gdrive_path):
+    path_or_url = get_path_or_url(path_or_gdrive_path)
+    if path_or_url in _cached_networks:
+        return _cached_networks[path_or_url]
+
+    if dnnlib.util.is_url(path_or_url):
+        stream = dnnlib.util.open_url(path_or_url, cache_dir='.stylegan2-cache')
+    else:
+        stream = open(path_or_url, 'rb')
+
+    tflib.init_tf()
+    with stream:
+        G, D, Gs = pickle.load(stream, encoding='latin1')
+    _cached_networks[path_or_url] = G, D, Gs
+    return G, D, Gs
+
+
+def generate_images_in_w_space(dlatents, truncation_psi):
+    Gs_kwargs = dnnlib.EasyDict()
+    Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    Gs_kwargs.randomize_noise = False
+    Gs_kwargs.truncation_psi = truncation_psi
+
+    imgs = []
+    for row, dlatent in enumerate(dlatents):
+        row_images = Gs.components.synthesis.run(dlatent, **Gs_kwargs)
+        imgs.append(row_images[0])
+    return imgs
+
+
+def iterate_batches(all_w, batch_size):
+    num_batches = (len(all_w) + batch_size - 1) // batch_size
+    for batch in range(num_batches):
+        w = all_w[batch * batch_size: (batch + 1) * batch_size]
+        yield w
+
+
+network_pkl = "gdrive:networks/stylegan2-ffhq-config-f.pkl"
+G, D, Gs = load_networks(network_pkl)
+
+Gs_syn_kwargs = dnnlib.EasyDict()
+
+Gs_syn_kwargs.output_transform = dict(
+    func=tflib.convert_images_to_uint8, nchw_to_nhwc=True
+)
+Gs_syn_kwargs.randomize_noise = False
+Gs_syn_kwargs.minibatch_size = 1
+
+noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
+
+all_w, all_a = load_dataset(keep=False, values=[0] * 17)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-num_epochs = 1000
+# AE Model
+model = AE_single_layer(input_shape=512, hidden_dim=100).to(device)
+criterion = torch.nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-2)
+
+start_epoch = 0
+end_epoch = 10
 batch_size = 32
-lr = args.lr
-balanced = True if args.values != "continuous" else False
-features = 8
 
-decay = args.decay
-sigma = args.sigma
-age_sigma = args.age_sigma
-if args.values == "2":
-    values = 2
-    all_w, all_a = load_dataset(values=values)
-elif args.values == "continuous":
-    values = "continuous"
-    all_w, all_a = load_dataset(values=args.values)
-else:
-    values = [int(args.values[i]) for i in range(len(args.values))]
-    all_w, all_a = load_dataset(values=[0] * 9 + values)
+for epoch in range(start_epoch, end_epoch):
+    all_w = shuffle(all_w)
+    model.train()
+    running_loss = 0.0
+    count_batches = 0
+    for w in iterate_batches(all_w, batch_size):
+        w = w.to(device)
+        optimizer.zero_grad()
 
-model_dir = f"nice_feat{features}_values{args.values}_decay{decay}_sigma{sigma}"
+        input_images = generate_images_in_w_space(w.cpu(), 1.0)
+        encoded, decoded = model(w)
 
-model_dir = f"saved/{model_dir}"
+        output_images = generate_images_in_w_space(encoded.cpu().detach().numpy(), 1.0)
+        loss = criterion(torch.FloatTensor(input_images), torch.FloatTensor(output_images))
+        loss = torch.autograd.Variable(loss, requires_grad=True)
 
-make_dir(model_dir)
+        loss.backward()
+        optimizer.step()
 
-zero_padding = torch.zeros(1, 1, 1).to(device)
-
-model = AE_single_layer(input_shape=512, hidden_dim=100)
-
-optimizer = optim.Adam(model.parameters(), lr=lr)
-
-styleflow = cnf(512, "512-512-512-512-512", 8, 1).to(device)
-optimizer_s = optim.Adam(styleflow.parameters(), lr=lr)
-
-styleflow, _ = load_model(
-    f"saved/original/feat8_normalized/model_e5.pch", styleflow, optimizer_s
-)
-
-values = "continuous"
-all_w, all_a = load_dataset(values=values)
-
-print(all_w.shape)
-
-# # Loading
-# start_epoch = args.start_epoch
-# if start_epoch > 0:
-#     model, optimizer = load_model(
-#         f"{model_dir}/model_e{start_epoch}.pch", model, optimizer
-#     )
-#
-# all_w, all_a = all_w[500:], all_a[500:]
-# print(all_w.shape, all_a.shape, values)
-# if values == 2:
-#     var_ratio = torch.sum((all_a[:, 9:17] > 0).float(), dim=0) / float(all_a.shape[0])
-# elif values != "continuous":
-#     var_ratio = []
-#     for i in range(len(values)):
-#         curr_var_ratio = torch.zeros(values[i])
-#         for v in range(values[i]):
-#             curr_var_ratio[v] = (
-#                     torch.sum(
-#                         (
-#                                 abs(all_a[:, 9 + i] - (-1 + v * (2.0 / (values[i] - 1)))) < 1e-3
-#                         ).float(),
-#                         dim=0,
-#                     )
-#                     / float(all_a.shape[0])
-#             )
-#         var_ratio += [curr_var_ratio]
-#
-# N_dist = Normal(torch.tensor([0.0]).to(device), torch.tensor([1.0]).to(device))
-#
-# for epoch in range(start_epoch, num_epochs):
-#     all_w, all_a = shuffle(all_w, all_a)
-#     model.train()
-#     running_loss, count_batches, loglike = 0.0, 0, 0.0
-#     for w, a in iterate_batches(all_w, all_a, batch_size):
-#         w, a = w.to(device)[:, 0, 0:1, :], a.to(device)[:, 9: 9 + features]
-#         cond = torch.zeros(a.shape[0], 1, 1, 1, device=device)
-#
-#         # Distribution
-#         current_sigma = sigma * (decay ** epoch) * torch.ones_like(a, device=device)
-#         if balanced:
-#             if values == 2:
-#                 sigmas_pos = current_sigma * torch.sqrt(2 * var_ratio).to(device)
-#                 sigmas_neg = current_sigma * torch.sqrt(2 * (1 - var_ratio)).to(device)
-#                 current_sigma = torch.where(a > 0, sigmas_pos, sigmas_neg)
-#             else:
-#                 for i in range(len(values)):
-#                     for v in range(values[i]):
-#                         sigma_v = current_sigma[:, i] * torch.sqrt(
-#                             sum(var_ratio[i] > 0) * var_ratio[i][v]
-#                         ).to(device)
-#                         current_sigma[:, i] = torch.where(
-#                             abs(a[:, i] - (-1 + v * (2.0 / (values[i] - 1)))) < 1e-3,
-#                             sigma_v,
-#                             current_sigma[:, i],
-#                         )
-#         if balanced and values == 2: current_sigma[:, 6] = age_sigma * (decay ** epoch)
-#         current_mean = a.unsqueeze(1).squeeze(-1)
-#         current_sigma = current_sigma.unsqueeze(1).squeeze(-1)
-#         a_dist = Normal(current_mean, current_sigma)
-#
-#         optimizer.zero_grad()
-#         z, logdet = model(w)
-#         z_attr = z[:, :, :features]
-#         z_rest = z[:, :, features:]
-#
-#         current_mean = a.unsqueeze(1).squeeze(-1)
-#         current_sigma = current_sigma.unsqueeze(1).squeeze(-1)
-#         a_dist = Normal(current_mean, current_sigma)
-#         logp_attr = a_dist.log_prob(z_attr)
-#         logp_rest = N_dist.log_prob(z_rest)
-#
-#         logpz = logp_attr.sum(-1, keepdim=True) + logp_rest.sum(-1, keepdim=True)
-#         loss = (logpz + logdet).mean()
-#         (-loss).backward()
-#         nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-#         optimizer.step()
-#
-#         running_loss += loss.item()
-#         loglike = torch.mean(logpz).item()
-#         count_batches += 1
-#         if (count_batches + 1) % 100 == 0:
-#             now = datetime.now()
-#             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-#             print(
-#                 f"{dt_string} | Epoch {epoch + 1} | Batch {count_batches + 1} | Loss {running_loss / count_batches:.4f} | Loglike {loglike / count_batches:.4f}"
-#             )
-#     mod = 50
-#     if (epoch + 1) % mod == 0: save_model(f"{model_dir}/model_e{epoch + 1}.pch", model, optimizer)
+        running_loss += loss.item()
+        count_batches += 1
+        if (count_batches + 1) % 100 == 0:
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print(
+                f"{dt_string} | Epoch {epoch + 1} | Batch {count_batches} | Loss {running_loss / count_batches:.4f}"
+            )
+    save_model(f"ae_model/model_e{epoch + 1}.pch", model, optimizer)
